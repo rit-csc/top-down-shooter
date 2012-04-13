96b6dae5718129eea13a7e81fd2e8b6980842e53
Tietokone <wtl4112@rit.edu>
2012-04-13 14:34:45 -0400

    Put enum in namespace

--- framework/model/CommonStructs.hpp
+++ framework/model/CommonStructs.hpp
@@ -1,12 +1,13 @@
 #ifndef __COMMON_STRUCTS_H__
 #define __COMMON_STRUCTS_H__
-
-enum Orientation {
-	North,
-	East,
-	South,
-	West,
-};
+namespace Orientation {
+	enum Orientation {
+		North,
+		East,
+		South,
+		West,
+	};
+}
 
 struct Location {
 	int mapRow;
